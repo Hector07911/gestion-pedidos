c7a506748709ff19529fbf3491829b548b082bfc
hector <bbc07911@gmail.com>
2026-09-04 09:05:47 -0600

    create Persona entity usando jakarta annotations

---
+++ src/main/java/com/gestion/demo/model/Persona.java
@@ -0,0 +1,53 @@
+package com.gestion.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "personas")
+public class Persona {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	private String telefono;
+	
+	public Persona() {
+		
+	}
+	
+	public Persona(String nombre, String telefono) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+}
